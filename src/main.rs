7c58f627fdf7f9f668fb257c611aef458eb47154
Eric Litwin <eric.litwin@pnmac.com>
2023-04-09 18:27:41 -0700

    activate the Player

--- src/main.rs
+++ src/main.rs
@@ -44,17 +44,23 @@ enum GameMode {
 }
 
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
