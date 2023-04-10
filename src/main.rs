9539d7c62b40a2ecd18b644e93618752406ac5f7
Eric Litwin <eric.litwin@pnmac.com>
2023-04-09 19:14:52 -0700

    render Obstacle and check for hit in main game loop

--- src/main.rs
+++ src/main.rs
@@ -90,7 +90,9 @@ enum GameMode {
 struct State {
     player: Player,
     frame_time: f32,
+    obstacle: Obstacle,
     mode: GameMode,
+    score: i32,
 }
 
 impl State {
@@ -98,7 +100,9 @@ impl State {
         State {
             player: Player::new(5, 25),
             frame_time: 0.0,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
             mode: GameMode::Menu,
+            score: 0,
         }
     }
 
@@ -151,7 +155,14 @@ impl State {
         }
         self.player.render(ctx);
         ctx.print(0, 0, "Press Space to flap!");
-        if self.player.y > SCREEN_HEIGHT {
+        ctx.print(0, 1, &format!("Score: {}", self.score));
+
+        self.obstacle.render(ctx, self.player.x);
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
             self.mode = GameMode::End;
         }
     }
